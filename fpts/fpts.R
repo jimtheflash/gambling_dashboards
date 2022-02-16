ed48fa94a4a374e505c374a5ce3410c277210493
jimtheflash <jim.kloet@gmail.com>
2022-02-15 19:07:28 -0600

    update with edge filtering

--- fpts/fpts.R
+++ fpts/fpts.R
@@ -8,7 +8,6 @@ library(jsonlite)
 library(shiny)
 library(DT)
 
-
 # ui ----------------------------------------------------------------------
 
 ui <- fluidPage(
@@ -35,24 +34,38 @@ server <- function(input, output, session) {
   # TODO is this restart.txt business still necessary
   if (file.exists('restart.txt')) system('touch restart.txt') else file.create('restart.txt')
   
+  # when the update button is pushed grab the latest odds_fpts.csv from github
   fpts_raw <- eventReactive(input$updateButton, {
     read.csv("https://raw.githubusercontent.com/jimtheflash/gambling_stuff/main/data/02_curated/nba_first_to_score/odds_fpts.csv")
   })
-  
+  # generate the output for the table
   fpts_tidy <- reactive({
+    # make a string with edges - this is not the right or best or even a good way to do it
     edges <- list()
     for (i in 1:nrow(fpts_raw())) {
-      test_row <- fpts_raw()[i, ]
+      test_row <- fpts_raw() %>%
+        filter(row_number() == i) 
       out_vec <- c()
-      if (!is.na(test_row$br_edge) & test_row$br_edge > 0) out_vec[['br']] <- paste0('br (+', test_row$br_line, ')')
-      if (!is.na(test_row$dk_edge) & test_row$dk_edge > 0)  out_vec[['dk']] <- paste0('dk (+', test_row$dk_line, ')')
-      if (!is.na(test_row$fd_edge) & test_row$fd_edge > 0)  out_vec[['fd']] <- paste0('fd (+', test_row$fd_line, ')')
-      if (!is.na(test_row$pb_edge) & test_row$pb_edge > 0)  out_vec[['pb']] <- paste0('pb (+', test_row$pb_line, ')')
-      if (length(out_vec) == 0) out_vec <- 'none'
-      edges[[i]] <- paste(out_vec, collapse = ', ')
+      if (!is.na(test_row$br_edge) & test_row$br_edge > 0) out_vec['br'] <- test_row$br_line
+      if (!is.na(test_row$dk_edge) & test_row$dk_edge > 0) out_vec['dk'] <- test_row$dk_line
+      if (!is.na(test_row$fd_edge) & test_row$fd_edge > 0) out_vec['fd'] <- test_row$fd_line
+      if (!is.na(test_row$pb_edge) & test_row$pb_edge > 0) out_vec['pb'] <- test_row$pb_line
+      if (all(is.na(out_vec))|is.null(out_vec)) {
+        edges[[i]] <- 'none'
+        } else {
+        sorted_edges <- rev(sort(out_vec[which(!is.na(out_vec))]))
+        out_string <- paste0(names(sorted_edges), ' +', sorted_edges, collapse = ', ')
+        edges[[i]] <- out_string
+        }
     }
+    
+    # return table with the edges and the nicely named columns
     fpts_raw() %>%
       mutate(Edges = unlist(edges)) %>%
+      rowwise() %>%
+      # NOTE: this suppressWarnings call is annoying but necessary
+      mutate(`Max Edge` = suppressWarnings(max(c_across(ends_with('edge')), na.rm = TRUE))) %>% 
+      ungroup() %>%
       group_by(team, game_date) %>%
       fill(game, .direction =  'downup') %>%
       ungroup() %>%
@@ -63,27 +76,39 @@ server <- function(input, output, session) {
         Game = game,
         Date = game_date,
         Expected = fpts_line,
-        Edges
+        `Max Edge`,
+        Edges,
+        BR = br_line,
+        DK = dk_line,
+        FD = fd_line,
+        PB = pb_line
       ) %>%
-      arrange(Expected)
+      arrange(desc(`Max Edge`))
   })
   
+  # when the update button is clicked, hit github api to find out datetime of latest commit for odds_fpts
   fpts_last_update <- eventReactive(input$updateButton, {
-    fpts_commit <- fromJSON("https://api.github.com/repos/jimtheflash/gambling_stuff/commits?path=data/02_curated/nba_first_to_score/odds_fpts.csv", flatten = TRUE)
-    max(as_datetime(fpts_commit$commit.committer.date))
+    max(as_datetime(fpts_raw()$run_timestamp_utc))
     })
-  
+  # extract and render datetime
   output$fpts_last_update <- renderText(
     paste0('FPTS Data Last Updated at ', fpts_last_update(), ' UTC')
     )
   
-  # make the table
+  # make the table of odds
   output$fpts_table <- renderDT(
     datatable(fpts_tidy(),
+              rownames = FALSE,
               options = list(
                 dom = 't',
                 paging = FALSE
-              )))
+              )) %>%
+      formatPercentage(columns = c('Max Edge'),
+                       digits = 2) %>%
+      formatCurrency(columns = c('Expected', 'BR', 'DK', 'FD', 'PB'),
+                     currency = "+",
+                     interval = 999,
+                     digits = 0))
 }
 
 # app ---------------------------------------------------------------------
