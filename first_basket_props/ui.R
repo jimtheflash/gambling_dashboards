5c6e9d723e394fab37ed4c42c1e1c350d47848cc
jimtheflash <jim.kloet@gmail.com>
2023-12-16 14:34:30 -0600

    fix instructions

--- first_basket_props/ui.R
+++ first_basket_props/ui.R
@@ -2,11 +2,17 @@ ui <-
   fluidPage(
     titlePanel('NBA First Basket Props!'),
     br(),
-    p(em('Thanks for taking a look! The projections are from a set of ML tools, and the odds are directly from the books. The edges shown here are the simple difference between the projected and offered odds. A positive edge is a value, and a negative edge indicates a bet is overpriced per our models.')),
-    br(),
-    p(em('Tables are sortable and filterable; to sort by multiple columns hold the Shift key. Table rows are grouped by player and team for non-exact bets. For exact bets, there are not any groups.')),
-    br(),
-    p(em('To update the data, clear your browser cache and refresh the page. Projections are updated a couple times an hour, and more frequently closer to tipoff. Projections start to drop off after the early games tipoff due to some timezone issues.')),
+    bsCollapse(
+      bsCollapsePanel(
+        title = 'Instructions (Click To Expand)',
+        p(em('Thanks for taking a look! The projections are from a set of ML tools, and the odds are directly from the books. The edges shown here are the simple difference between the projected and offered odds. A positive edge is a value, and a negative edge indicates a bet is overpriced per our models.')),
+        br(),
+        p(em('Tables are sortable and filterable; to sort by multiple columns hold the Shift key. Table rows are grouped by player and team for non-exact bets. For exact bets, there are not any groups.')),
+        br(),
+        p(em('To update the data, clear your browser cache and refresh the page. Projections are updated a couple times an hour, based on the latest odds we pull and any lineup changes.'))),
+      open = FALSE),
+
+
     # br(),
     # fluidRow(column(width = 6,
     #                 bsCollapse(
